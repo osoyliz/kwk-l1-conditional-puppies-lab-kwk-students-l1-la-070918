f4415d3fea51529757a98a4672356284f3e5402f
osoyliz <lizosoy18@gmail.com>
2018-07-10 21:12:05 +0000

    automatically backed up by learn

--- gub.rb
+++ gub.rb
@@ -7,3 +7,15 @@ def no_puppies
   end
 end
 no_puppies
+
+x = 6
+def less_puppies
+  if x < 3
+    puts "Good job!"
+  elsif x == 3
+    puts "Slow down, Rachel!"
+  else x > 3
+    puts "Get back onto your animal restriction, RACHEL!"
+  end
+end
+less_puppies
