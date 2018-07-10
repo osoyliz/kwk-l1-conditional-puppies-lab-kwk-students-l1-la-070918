6cdcda2a7845676b98c040d937b8e62d2ab353b2
osoyliz <lizosoy18@gmail.com>
2018-07-10 21:34:01 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -1,6 +1,6 @@
 # 1. Write a method called no_puppies that takes in a number of puppies as an argument. If the number of puppies is greater than zero then this method should return "No more puppies!" otherwise it should return "Well done, Rachel!"
 
-x = 3
+
 def no_puppies
   if x > 0
     puts "No more puppies!"
